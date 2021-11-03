e12279f3574b016dd0a27f18b4237e9541a4fa65
Dzulfiqar Fathur Rahman <frdzulfiqar@gmail.com>
2021-11-03 23:07:21 +0700

    Minor improvements
    Add a parameter to customize the base size for line elements.
    Previously, the function only modifies the size of major grid lines.
    
    Use `ggtext::element_markdown()` to add more flexibility in customizing
    text elements. This is especially helpful for title, subtitle and
    caption.
    
    Add a chunk to customize legend title, text, key, margin and background.
    
    Minor improvements for axis text and titles.

--- R/dfr_theme.R
+++ R/dfr_theme.R
@@ -1,10 +1,10 @@
 #' Apply custom theme
 #' 
-#' @param text_size_base The base text size in pts.
+#' @param text_size_base The base text size in points (pt). Defaults to 12 pts.
 #' @param text_color_primary The text color. Defaults to #757575, except 
 #'   for plot title and caption.
 #' @param axis_line_color The axis line color. Defaults to #E0E0E0.
-#' @param grid_major_lwd The base size for major grid lines.
+#' @param line_size_base The base size for lines.
 #' @param plot_margin The base size for plot margins on the top, right, bottom
 #'   and left sides in cm. Defaults to 0.25 cm.
 #'
@@ -14,71 +14,82 @@
 dfr_theme <- function(text_size_base = 12,
                       text_color_primary = "#757575",
                       axis_line_color = "#E0E0E0",
-                      grid_major_lwd = text_size_base / 22,
+                      line_size_base = text_size_base / 22,
                       plot_margin = 0.25) {
   
-  ggplot2::theme_grey(base_size = text_size_base) %+replace%
-    theme(
-      text = element_text(
+  ggplot2::theme_grey() %+replace%
+    ggplot2::theme(
+      text = ggplot2::element_text(
         size = text_size_base, 
         color = text_color_primary
       ),
-      axis.title = element_text(size = rel(1)),
-      axis.text = element_text(
-        size = rel(1),
+      line = ggplot2::element_line(size = line_size_base),
+      axis.title.x = ggtext::element_markdown(margin = ggplot2::margin(t = 5)),
+      axis.title.y = ggtext::element_markdown(
+        angle = 90,
+        margin = ggplot2::margin(r = 5)
+      ),
+      axis.title.y.right = ggtext::element_markdown(
+        angle = -90,
+        margin = ggplot2::margin(l = 5)
+      ),
+      axis.text = ggplot2::element_text(
+        size = ggplot2::rel(0.9166667),
         color = text_color_primary
       ),
-      plot.title = element_text(
+      axis.line = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      plot.title = ggtext::element_markdown(
         face = "bold",
         hjust = 0,
-        size = rel(1.166667),
+        size = ggplot2::rel(1.166667),
         color = "#000000",
-        margin = margin(b = 5)
+        margin = ggplot2::margin(b = 5)
       ),
-      plot.subtitle = element_text(
+      plot.subtitle = ggtext::element_markdown(
         hjust = 0,
-        size = rel(1.041667),
+        size = ggplot2::rel(1.041667),
         color = text_color_primary,
-        margin = margin(b = 10)
+        margin = ggplot2::margin(b = 10)
       ),
       plot.title.position = "plot",
-      plot.caption = element_text(
+      plot.caption = ggtext::element_markdown(
         hjust = 0,
-        size = rel(0.8333333),
+        size = ggplot2::rel(0.875),
         color = "#9E9E9E",
-        margin = margin(t = 10)
+        margin = ggplot2::margin(t = 10)
       ),
       plot.caption.position = "plot",
-      strip.text = element_text(
+      strip.text = ggplot2::element_text(
         face = "bold",
         hjust = 0,
         size = rel(1)
       ),
-      strip.background = element_blank(),
-      panel.background = element_rect(
+      strip.background = ggplot2::element_blank(),
+      legend.text = ggplot2::element_text(size = ggplot2::rel(0.9166667)),
+      legend.title = ggplot2::element_blank(),
+      legend.key = ggplot2::element_blank(),
+      legend.background = ggplot2::element_blank(),
+      legend.margin = ggplot2::margin(0),
+      panel.background = ggplot2::element_rect(
         fill = "#FFFFFF", 
         color = NA
       ),
-      panel.spacing = unit(1, "lines"),
-      panel.grid.minor = element_blank(),
-      panel.grid.major = element_line(
-        size = grid_major_lwd,
-        color = axis_line_color
-      ),
-      panel.border = element_blank(),
-      plot.background = element_rect(
+      panel.spacing = ggplot2::unit(1, "lines"),
+      panel.grid.minor = ggplot2::element_blank(),
+      panel.grid.major = ggplot2::element_line(color = axis_line_color),
+      panel.border = ggplot2::element_blank(),
+      plot.background = ggplot2::element_rect(
         fill = "#FFFFFF",
         color = NA
       ),
-      plot.margin = margin(
+      plot.margin = ggplot2::margin(
         t = plot_margin, 
         r = plot_margin, 
         b = plot_margin, 
         l = plot_margin, 
         unit = "cm"
-      ),
-      axis.line = element_blank(),
-      axis.ticks = element_blank()
+      )
     )
   
 }
