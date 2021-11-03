ff5bfc87da974a871be670b5ab15ccd2aee33b5b
Dzulfiqar Fathur Rahman <frdzulfiqar@gmail.com>
2021-11-03 23:22:15 +0700

    Add function to convert font size

---
+++ R/convert_font_size.R
@@ -0,0 +1,10 @@
+#' Convert font size 
+#'
+#' @param text_size_base The base text size in points (pt). Defaults to 12 pts. 
+#' @param text_size_rel The default relative size for text elements.
+#'
+#' @export
+convert_font_size <- function(text_size_base = 12,
+                              text_size_rel = 0.9166667) {
+  text_size_base * text_size_rel / ggplot2::.pt
+}
